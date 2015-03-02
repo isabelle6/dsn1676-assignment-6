fd0b94e67a3470ad4b8845d31674c0b64c0efeb0
Isabelle6 <isabelle-fortier@hotmail.com>
2015-03-02 18:20:27 -0500

    Animated first three elements
    Made the show/hide button show and hide the box using a click function
    in js. Created a class in css and added to an existing class.
    Made the move in/out button move the diamond in and out by using a
    click function in js. Created a class in css and added to two existing
    classes.
    Made the collapse/expand button collapse and expand the box using a
    click function in js. Created a class in css and added to an existing
    class.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var $btnShow = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+var $btnCollapse = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+$btnShow.on('click', function() {
+    $box.toggleClass('js-box-show-hide');
+});
+
+$btnMove.on('click', function() {
+    $diamond.toggleClass('js-diamond-move');
+});
+
+$btnCollapse.on('click', function() {
+    $panel.toggleClass('js-panel-collapse-expand');
+});
